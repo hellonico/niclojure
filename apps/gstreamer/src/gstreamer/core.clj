c6091338e2aa137efc5ac447b6a201bd9cdb4f72
Nicolas Modrzyk <hellonico@gmail.com>
2012-10-25 11:00:44 +0900

    gstreamer just in case

---
+++ apps/gstreamer/src/gstreamer/core.clj
@@ -0,0 +1,1 @@
+(ns gstreamer.core)
---
+++ apps/gstreamer/test/gstreamer/test/core.clj
@@ -0,0 +1,6 @@
+(ns gstreamer.test.core
+  (:use [gstreamer.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
