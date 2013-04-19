d47844bfbde4552473a2ba62c43d1ec0c664505a
Nicolas Modrzyk <nico@fivecool.net>
2013-04-19 19:07:20 +0900

    wip

---
+++ apps/algo_meteor/project.clj
@@ -0,0 +1,7 @@
+(defproject algo_meteor "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main algo-meteor.core
+  :dependencies [[org.clojure/clojure "1.5.1"]])
